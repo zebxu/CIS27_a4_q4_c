28c7c7b1079e53811a2579d067814bbf33c9b0ce
zeyang xu <zeyangxu@zeyangs-MacBook-Pro.local>
2018-05-24 18:58:28 -0700

    comment the redundant codes

--- src/DijkstraSP.java
+++ src/DijkstraSP.java
@@ -209,28 +209,28 @@ public class DijkstraSP {
      *
      * @param args the command-line arguments
      */
-    public static void main(String[] args) {
-        In in = new In("/Users/zeyangxu/Java Projects/ShortestPath/src/graph_3_c");
-        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
-        int s = Integer.parseInt(args[1]);
-
-        // compute shortest paths
-        DijkstraSP sp = new DijkstraSP(G, s);
-
-
-        // print shortest path
-        for (int t = 0; t < G.V(); t++) {
-            if (sp.hasPathTo(t)) {
-                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
-                for (DirectedEdge e : sp.pathTo(t)) {
-                    StdOut.print(e + "   ");
-                }
-                StdOut.println();
-            }
-            else {
-                StdOut.printf("%d to %d         no path\n", s, t);
-            }
-        }
-    }
+//    public static void main(String[] args) {
+//        In in = new In("/Users/zeyangxu/Java Projects/ShortestPath/src/graph_3_c");
+//        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+//        int s = Integer.parseInt(args[1]);
+//
+//        // compute shortest paths
+//        DijkstraSP sp = new DijkstraSP(G, s);
+//
+//
+//        // print shortest path
+//        for (int t = 0; t < G.V(); t++) {
+//            if (sp.hasPathTo(t)) {
+//                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+//                for (DirectedEdge e : sp.pathTo(t)) {
+//                    StdOut.print(e + "   ");
+//                }
+//                StdOut.println();
+//            }
+//            else {
+//                StdOut.printf("%d to %d         no path\n", s, t);
+//            }
+//        }
+//    }
 
 }
